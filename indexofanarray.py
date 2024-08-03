399a0900e79d5ecbac905908f5badec7109dbbeb
sandhya <145090559+sandhyaakkamgari@users.noreply.github.com>
2024-08-03 23:15:34 +0530

    Add files via upload

---
+++ indexofanarray.py
@@ -0,0 +1,12 @@
+#numbers list
+array = [1,2,5,6,8]
+
+# index of 'e' in vowels
+index = array.index(2)
+print('The index of 2:', index)
+
+# element 'i' is searched
+# index of the first 'i' is returned
+index = array.index(6)
+
+print('The index of 6:', index)
